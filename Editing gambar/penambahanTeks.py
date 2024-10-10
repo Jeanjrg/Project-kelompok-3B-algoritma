0efe0f157d272cb63d4ffc86e7f28d86c5cebdc6
Jean Patra Paeloran <jeanbumbungan@gmail.com>
2024-10-10 11:23:46 +0800

    Update penambahanTeks.py

--- Editing gambar/penambahanTeks.py
+++ Editing gambar/penambahanTeks.py
@@ -1,40 +1,17 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def tambahkan_teks(input_path,output_path,teks,left,top,nama_font,font_size,warna_font):
-    """
-    Menambahkan teks ke gambar pada koordinat yang ditentukan dan menyimpannya.
-
-    Fungsi ini menerima jalur gambar input, teks yang akan ditambahkan, koordinat
-    kiri atas (left, top) untuk menempatkan teks, nama font, ukuran font, dan warna font.
-    Teks kemudian akan ditempatkan di posisi yang ditentukan dan gambar hasilnya disimpan 
-    di jalur output yang diberikan.
-
-    Parameter:
-    -----------
-    input_path : str
-        Jalur file gambar input.
-    output_path : str
-        Jalur file untuk menyimpan gambar dengan teks yang ditambahkan.
-    teks : str
-        Teks yang akan ditambahkan pada gambar.
-    left : int
-        Posisi horizontal (kiri) teks dalam piksel.
-    top : int
-        Posisi vertikal (atas) teks dalam piksel.
-    nama_font : str
-        Nama file font (TrueType) yang akan digunakan untuk teks.
-    font_size : int
-        Ukuran font teks.
-    warna_font : str
-        Warna teks yang akan ditambahkan dalam format heksadesimal atau nama warna yang didukung.
-
-    Returns:
-    --------
-    Tidak ada nilai yang dikembalikan, tetapi gambar hasil akan disimpan di jalur output yang ditentukan.
-
-    Catatan:
-    - Pastikan nama file font yang digunakan valid dan dapat diakses oleh program.
-    - Gambar yang telah diubah akan ditampilkan dan disimpan di jalur output yang ditentukan.
+    """Menambahkan teks ke gambar pada koordinat yang ditentukan dan menyimpannya.
+    
+    Args:
+        Input_path (str): Jalur file gambar input.
+        output_path (str): Jalur file untuk menyimpan gambar dengan teks yang ditambahkan.
+        teks (str): Teks yang akan ditambahkan pada gambar.
+        left (int): Posisi horizontal (kiri) teks dalam piksel.
+        top (int): Posisi vertikal (atas) teks dalam piksel.
+        nama_font (str): Nama file font (TrueType) yang akan digunakan untuk teks.
+        font_size (int): Ukuran font teks.
+        warna_font (str): Warna teks yang akan ditambahkan dalam format heksadesimal atau nama warna yang didukung.
     """
     # Membuka file gambar
     input_path = Image.open(input_path)
