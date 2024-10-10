f5c15a91a0acccc577cc063c0b5e73f8fa898097
Jean Patra Paeloran <jeanbumbungan@gmail.com>
2024-10-10 10:22:14 +0800

    Update convert_to_grayscale.py

--- Editing gambar/convert_to_grayscale.py
+++ Editing gambar/convert_to_grayscale.py
@@ -1,6 +1,28 @@
 from PIL import Image
 
 def convert_image_to_grayscale(input_path,output_path):
+    """
+    Mengonversi gambar berwarna menjadi gambar grayscale dan menyimpannya.
+
+    Fungsi ini menerima jalur file gambar berwarna sebagai input, mengonversinya
+    menjadi gambar grayscale, dan menyimpan gambar hasil di jalur yang ditentukan.
+
+    Parameter:
+    -----------
+    input_path : str
+        Jalur file gambar input yang akan diubah menjadi grayscale.
+    output_path : str
+        Jalur file tempat gambar grayscale akan disimpan.
+
+    Catatan:
+    - Jika terjadi kesalahan saat membuka gambar, fungsi akan menampilkan pesan kesalahan.
+    - Setelah gambar berhasil diubah menjadi grayscale, gambar hasil akan disimpan dan ditampilkan.
+
+    Returns:
+    --------
+    Tidak ada nilai yang dikembalikan secara eksplisit, tetapi gambar hasil akan disimpan 
+    di jalur output yang ditentukan.
+    """
     # Buka gambar
     try:
         image = Image.open(input_path)
@@ -18,7 +40,7 @@ def convert_image_to_grayscale(input_path,output_path):
     # Menunjukkan hasil gambar
     grayscale_image.show()
 
-convert_image_to_grayscale(
-    "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
-    "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser14.png" # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
-)
+# convert_image_to_grayscale(
+#     "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
+#     "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser14.png" # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
+# )
