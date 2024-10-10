c6e0415d519b378ab9a42bb8a0b529a6c60129c5
Jean Patra Paeloran <jeanbumbungan@gmail.com>
2024-10-10 13:43:18 +0800

    Update pemotongan_gambar.py

--- Editing_gambar/pemotongan_gambar.py
+++ Editing_gambar/pemotongan_gambar.py
@@ -12,15 +12,15 @@ def ukuran_gambar(Input_path,Output_path,left,top,right,bottom):
         bottom (int): Koordinat batas bawah gambar yang ingin dipotong (dalam piksel).
     """
     try:
-        # Membuka file gambar
-        image = Image.open(Input_path)
-        cropped_image = image.crop((left, top, right, bottom)) # Memotong gambar sesuai koordinat kiri atas dan kanan bawah sehingga terbentuk segii empat
-        # Menyimpan hasil di jalur output
-        cropped_image.save(Output_path)
-        cropped_image.show() # Menunjukkan hasil gambar yang telah dipotong
-        return f"Gambar berhasil dipotong dan disimpan di {Output_path}" # Akhir dari program
+         # Membuka file gambar
+         image = Image.open(Input_path)
+         cropped_image = image.crop((left, top, right, bottom)) # Memotong gambar sesuai koordinat kiri atas dan kanan bawah sehingga terbentuk segii empat
+         # Menyimpan hasil di jalur output
+         cropped_image.save(Output_path)
+         cropped_image.show() # Menunjukkan hasil gambar yang telah dipotong
+         return f"Gambar berhasil dipotong dan disimpan di {Output_path}" # Akhir dari program
     except Exception as e:
-        return f"Terjadi kesalahan: {e}"
+         return f"Terjadi kesalahan: {e}"
 
 # ukuran_gambar(
 #               "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
