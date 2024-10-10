ebc3da1626781be4f0c4c49a5dafa310187b82b6
Jean Patra Paeloran <jeanbumbungan@gmail.com>
2024-10-10 10:20:10 +0800

    Update Brightness.py

--- Editing gambar/Brightness.py
+++ Editing gambar/Brightness.py
@@ -1,8 +1,41 @@
 from PIL import Image, ImageEnhance
 import os
 
-def adjust_brightness(image_path,output_path,level):
-    """Mengubah kecerahan gambar berdasarkan level yang ditentukan."""
+def adjust_brightness(image_path,output_path,level=1):
+    """
+    Mengubah kecerahan gambar berdasarkan level yang ditentukan dan menyimpan hasilnya.
+
+    Fungsi ini menerima jalur file gambar, jalur keluaran untuk gambar hasil, 
+    dan level kecerahan yang diinginkan. Kecerahan gambar akan diatur 
+    berdasarkan level yang ditentukan, dengan pilihan level sebagai berikut:
+    
+    - 'sangat gelap': 0.2
+    - 'gelap': 0.5
+    - 'normal': 1.0 (default)
+    - 'cerah': 1.5
+    - 'sangat cerah': 2.0
+    
+    Jika level yang diberikan tidak dikenali, fungsi akan menggunakan nilai 
+    kecerahan default (1.0 - normal).
+
+    Parameter:
+    -----------
+    image_path : str
+        Jalur file gambar input yang akan diubah kecerahannya.
+    output_path : str
+        Jalur file tempat gambar hasil akan disimpan.
+    level : str
+        Level kecerahan yang diinginkan (misalnya 'gelap', 'cerah', dll.).
+
+    Catatan:
+    - Fungsi ini akan menampilkan pesan kesalahan jika file gambar tidak ditemukan.
+    - Gambar hasil akan disimpan dan ditampilkan setelah kecerahan diubah.
+
+    Returns:
+    --------
+    Tidak ada nilai yang dikembalikan secara eksplisit, tetapi gambar hasil 
+    akan disimpan di jalur output yang ditentukan.
+    """
     # Faktor kecerahan berdasarkan level yang ditentukan
     brightness_factors = {
         'sangat gelap': 0.2,
@@ -26,8 +59,8 @@ def adjust_brightness(image_path,output_path,level):
     brightened_image.save(output_path) # Menyimpan hasil di jalur output
     brightened_image.show(output_path) # Menunjukkan hasil pencerahan gambar
 
-adjust_brightness(
-    "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
-    "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser14.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
-    "sangat gelap" # Pilihan level yang tersedia: sangat gelap, gelap, normal, cerah, sangat cerah
-)
+# adjust_brightness(
+#     "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
+#     "C:\\Users\jpael\OneDrive\Pictures\Ignition Teaser14.png", # Buat backslash (\) menjadi double backslash (\\) agar path gambar dapat terbaca
+#     "sangat gelap" # Pilihan level yang tersedia: sangat gelap, gelap, normal, cerah, sangat cerah
+# )
