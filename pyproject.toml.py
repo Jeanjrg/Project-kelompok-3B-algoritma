7bb51d4dde972c35944087f6a46d039e064aed43
Jean Patra Paeloran <jeanbumbungan@gmail.com>
2024-10-10 13:31:58 +0800

    Update pyproject.toml.py

--- pyproject.toml.py
+++ pyproject.toml.py
@@ -3,7 +3,7 @@ requires = ["setuptools>=42", "wheel"]
 build-backend = "setuptools.build_meta"
 
 [project]
-name = "Editing gambar"  # Nama proyek sebaiknya tidak mengandung spasi
+name = "Editing_gambar"  # Nama proyek sebaiknya tidak mengandung spasi
 version = "0.1.0"
 description = "Paket Python untuk pengolahan gambar"
 authors = ["jean <email@jeanbumbungan.com>"]
@@ -18,4 +18,4 @@ numpy = "^1.21"
 Pillow = "^8.0"
 
 [tool.setuptools.packages.find]
-where = ["Editing gambar"]  # Pastikan direktori ini ada di struktur paket Anda
+where = ["Editing_gambar"]  # Pastikan direktori ini ada di struktur paket Anda
