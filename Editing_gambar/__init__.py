078629b3046cb989722a4a8efae93b7abc6e7949
Jean Patra Paeloran <jeanbumbungan@gmail.com>
2024-10-11 14:30:40 +0800

    Update __init__.py

--- Editing_gambar/__init__.py
+++ Editing_gambar/__init__.py
@@ -1,9 +1,9 @@
 from .hitam_putih import hitam_putih
-from .rotasiGambar import RotasiGambar
-from .Kecerahan import kecerahan
+from .rotasiGambar import rotasiGambar
+from .Kecerahan import Kecerahan
 from .penambahanTeks import penambahanTeks
 from .pencerminanGambar import pencerminanGambar
-from .pemotonganGambar import PemotonganGambar
+from .pemotonganGambar import pemotonganGambar
 
 __all__ = [
     'convert_image_to_grayscale',
